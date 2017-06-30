d9415cc1702284fcb36f6af26d5d9e8b7dc35a7e
Bruno Luiz <contact@brunoluiz.net>
2017-06-29 22:06:53 -0300

    Adds expressjs

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+const express = require('express')
+
+require('dotenv').config()
+const port = process.env.PORT
+
+let app = express()
+
+app.listen(port, () => console.log('Up and running!'))
