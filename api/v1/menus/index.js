0c6a3deac3f1472025b4d4ae3da35e4d3a77ee46
Bruno Luiz <contact@brunoluiz.net>
2017-06-29 23:36:14 -0300

    Adds default behavior to the menus routes

--- api/v1/menus/index.js
+++ api/v1/menus/index.js
@@ -1,16 +1,18 @@
 const router = require('express').Router()
 
 router.get('/:location', (req, res, next) => {
-  res.sendStatus(200)
+  res.sendstatus(200)
   next()
 })
 
 router.post('/collect', (req, res, next) => {
-
+  res.sendstatus(200)
+  next()
 })
 
 router.post('/collect/:location', (req, res, next) => {
-
+  res.sendstatus(200)
+  next()
 })
 
 module.exports = router
