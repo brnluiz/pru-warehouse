fa79ff9eebee82cc3ee830b39a0a95f5304386db
Bruno Luiz <contact@brunoluiz.net>
2017-07-01 18:38:43 -0300

    Adds a stream for test env

--- helpers/log.js
+++ helpers/log.js
@@ -1,8 +1,19 @@
 const bunyan = require('bunyan')
 const name = require('../package.json').name
+require('dotenv').config()
+
+let streams = []
+
+if (process.env.NODE_ENV === 'test') {
+  streams.push({
+    type: 'file',
+    path: `/tmp/${name}.log`
+  })
+}
 
 const log = bunyan.createLogger({
   name,
+  streams,
   level: process.env.LOG_LEVEL || 'trace'
 })
 
