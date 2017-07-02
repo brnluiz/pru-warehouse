b03e507208702de4d5e7ef28e5d055cff1922e55
Bruno Luiz <contact@brunoluiz.net>
2017-07-02 00:39:09 -0300

    Removes location by date (will be passed as argument on /menus/:location)

--- api/v1/menus/controllers/menus-get-by-location-and-date-controller.js
+++
@@ -1,6 +0,0 @@
-const menusGetByLocationAndDate = (req, res, next) => {
-  res.sendStatus(200)
-  next()
-}
-
-module.exports = menusGetByLocationAndDate
--- api/v1/menus/index.js
+++ api/v1/menus/index.js
@@ -1,12 +1,8 @@
 const router = require('express').Router()
 
 const getByLocationController = require('./controllers/menus-get-by-location-controller')
-const getByLocationAndDateController = require('./controllers/menus-get-by-location-and-date-controller')
 
 // TODO: returns the menu for a specific location
 router.get('/:location', getByLocationController)
-
-// TODO: returns the menu for a specific location and date
-router.get('/:location/:date', getByLocationAndDateController)
 
 module.exports = router
