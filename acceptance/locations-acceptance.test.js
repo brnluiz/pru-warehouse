94f177de30c7abed731cbe3aa981e790dc331dab
Bruno Luiz <contact@brunoluiz.net>
2017-07-03 22:35:18 -0300

    First tries on await/async

--- acceptance/locations-acceptance.test.js
+++ acceptance/locations-acceptance.test.js
@@ -1,29 +1,40 @@
 const app = require('../app')
 const request = require('supertest')(app)
 const test = require('tape')
+const db = require('../db')
 
-test('create a collection request', t =>
-  request
-    .post('/locations/collect')
-    .set('Accept', 'application/json')
-    .expect(201)
-    .end((err, res) => {
-      t.equals(err, null)
-      t.same(res.body, {})
-      t.end()
-    })
-)
+const beforeAll = async () => {
+  await db.location.sync({ force: true })
+  await db.location.create({
+    name: 'test',
+    price: 1.99
+  })
+}
 
-test('execute a collect on the specified location', t =>
-  request
-    .post('/locations/collect/testLocation')
-    .set('Accept', 'application/json')
-    .expect(201)
-    .end((err, res) => {
-      t.equals(err, null)
-      t.same(res.body, {})
-      t.end()
-    })
-)
+(async () => {
+  await beforeAll()
 
-test.onFinish(() => process.exit(0))
+  test('create a collection request', t =>
+    request
+      .post('/locations/collect')
+      .set('Accept', 'application/json')
+      .expect(201)
+      .end((err, res) => {
+        t.equals(err, null)
+        t.same(res.body, {})
+        t.end()
+      })
+  )
+
+  test('execute a collect on the specified location', t =>
+    request
+      .post('/locations/collect/testLocation')
+      .set('Accept', 'application/json')
+      .expect(201)
+      .end((err, res) => {
+        t.equals(err, null)
+        t.same(res.body, {})
+        t.end()
+      })
+  )
+})()
