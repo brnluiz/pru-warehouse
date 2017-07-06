6db01297e6ce28d01a706f8522394819ab420a23
Bruno Luiz <contact@brunoluiz.net>
2017-07-06 20:24:34 -0300

    Improves logs

--- helpers/log.js
+++ helpers/log.js
@@ -2,19 +2,16 @@ const bunyan = require('bunyan')
 const name = require('../package.json').name
 require('dotenv').config()
 
-let streams = []
+const log = bunyan.createLogger({
+  name,
+  level: process.env.LOG_LEVEL || 'trace'
+})
 
 if (process.env.NODE_ENV === 'development') {
-  streams.push({
+  log.addStream({
     type: 'file',
     path: `/tmp/${name}.log`
   })
 }
-
-const log = bunyan.createLogger({
-  name,
-  streams,
-  level: process.env.LOG_LEVEL || 'trace'
-})
 
 module.exports = log
