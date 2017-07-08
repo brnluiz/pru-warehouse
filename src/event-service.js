5a0ae4cf3aa28c79f216d45f9ffab0a69866a782
Bruno Luiz <contact@brunoluiz.net>
2017-07-08 12:47:22 -0300

    Adds more info to event-service message

--- src/event-service.js
+++ src/event-service.js
@@ -4,8 +4,17 @@ const tag = 'event-service'
 const handlers = {}
 
 const EventService = {
-  async emit (event, payload) {
-    log.info(`[${tag}] ${event} received`, payload)
+  async emit (event, current, previous) {
+    const message = {
+      current,
+      previous,
+      createdAt: new Date()
+    }
+
+    log.info(`[${tag}] ${event} received`, message)
+
+    handlers[event].forEach(handler =>
+      handler(message, event))
   },
   async on (event, handler) {
     handlers[event]
